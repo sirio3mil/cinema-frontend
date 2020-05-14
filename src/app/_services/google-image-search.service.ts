4ff0e5bdf66ab252237540fd05e1ac88841c96eb
Reynier de la Rosa <sirio3mil@gmail.com>
2020-05-14 19:46:58 +0200

    add google search service

---
+++ src/app/_services/google-image-search.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+const searchEngineKey = environment.searchEngineKey;
+const googleApiKey = environment.googleApiKey;
+const googleSearchEndpoint = environment.googleSearchEndpoint;
+
+@Injectable({providedIn: 'root'})
+export class GoogleImageSearchService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(param: string) {
+    return this.http.get<any>(`${googleSearchEndpoint}?key=${googleApiKey}&cx=${searchEngineKey}&q=${param}&imgSize=large`);
+  }
+}
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,24 +1,25 @@
-﻿import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
+﻿import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
-import { User } from '../_models';
+import {User} from '../_models';
 
 const apiEndpoint = environment.apiEndpoint;
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class UserService {
-    constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-    getAll() {
-        return this.http.get<User[]>(`${apiEndpoint}/users`);
-    }
+  getAll() {
+    return this.http.get<User[]>(`${apiEndpoint}/users`);
+  }
 
-    register(user: User) {
-        return this.http.post(`${apiEndpoint}/users/register`, user);
-    }
+  register(user: User) {
+    return this.http.post(`${apiEndpoint}/users/register`, user);
+  }
 
-    delete(id: number) {
-        return this.http.delete(`${apiEndpoint}/users/${id}`);
-    }
+  delete(id: number) {
+    return this.http.delete(`${apiEndpoint}/users/${id}`);
+  }
 }
