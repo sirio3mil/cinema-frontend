c59425eb01fac3a96bda7c6e610959174808ff06
Reynier de la Rosa <sirio3mil@gmail.com>
2020-05-18 00:00:26 +0200

    remove duplicate code

--- src/app/import-imdb-movie/import-imdb-movie.component.ts
+++ src/app/import-imdb-movie/import-imdb-movie.component.ts
@@ -1,10 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {AlertService} from '../_services';
 import {Tape} from '../_models';
-import {faPlusCircle, faCheckCircle, faFileImport} from '@fortawesome/free-solid-svg-icons';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {EditTapeUserComponent} from '../edit-tape-user';
 import {ImportImdbMovieGQL} from '../_gql';
 
 @Component({templateUrl: 'import-imdb-movie.component.html'})
@@ -12,17 +8,13 @@ export class ImportImdbMovieComponent implements OnInit {
 
   importing = false;
   submitted = false;
-  faPlusCircle = faPlusCircle;
-  faFileImport = faFileImport;
 
   tape: Tape;
   importForm: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
-    private alertService: AlertService,
-    private importImdbMovieService: ImportImdbMovieGQL,
-    private ngbModal: NgbModal
+    private importImdbMovieService: ImportImdbMovieGQL
   ) {
   }
 
@@ -32,16 +24,12 @@ export class ImportImdbMovieComponent implements OnInit {
     });
   }
 
-  // convenience getter for easy access to form fields
   get f() {
     return this.importForm.controls;
   }
 
   onSubmit() {
     this.submitted = true;
-    // reset alerts on submit
-    this.alertService.clear();
-    // stop here if form is invalid
     if (this.importForm.invalid) {
       return;
     }
@@ -54,17 +42,6 @@ export class ImportImdbMovieComponent implements OnInit {
         this.importing = false;
         this.submitted = false;
         this.tape = result.data.importImdbMovie;
-      });
-  }
-
-  addTapeUser() {
-    const modalRef = this.ngbModal.open(EditTapeUserComponent);
-    modalRef.componentInstance.title = this.tape.originalTitle;
-    modalRef.componentInstance.tapeId = this.tape.tapeId;
-    modalRef.result
-      .then(result => {
-        this.tape.tapeUser = result;
-        this.faPlusCircle = faCheckCircle;
       });
   }
 }
